b2e83e0dc49d26c24ab49ae0221b6553d6a0f94d
xagriff <xagriffith@gmail.com>
2016-08-20 18:19:44 -0400

    Added GMST and LST functions
    Added functions for calculating GMST and LST (Algorithm 15) and
    corresponding test function (Example 3-5)

--- astro_time.py
+++ astro_time.py
@@ -6,6 +6,7 @@ Created on Sat Aug 20 17:34:45 2016
 """
 
 import numpy as np
+import math
 
 def julian_date(yr, mo, d, hr, minute, sec, leap_sec=False):
     x = (7*(yr + np.trunc((mo + 9)/12)))/4.0
@@ -17,3 +18,16 @@ def julian_date(yr, mo, d, hr, minute, sec, leap_sec=False):
     z = (sec/t + minute)/60.0 + hr
     jd = 367*yr - np.trunc(x) + np.trunc(y) + d + 1721013.5 + z/24.0
     return jd
+    
+def find_gmst(jd_ut1):
+    t_ut1 = (jd_ut1 - 2451545.0)/36525.0
+    theta_gmst = 67310.54841 + (876600*3600.0 + 8640184.812866)*t_ut1 + 0.093104*t_ut1*t_ut1 - 6.2E-6*t_ut1*t_ut1*t_ut1
+    theta_gmst = math.fmod(theta_gmst, 86400.0)
+    theta_gmst /= 240.0
+    if theta_gmst < 0:
+        theta_gmst += 360.0
+    return theta_gmst
+    
+def find_lst(theta_gmst, lon):
+    theta_lst = theta_gmst + lon
+    return theta_lst
--- test_time.py
+++ test_time.py
@@ -21,9 +21,28 @@ class TimeExamplesFromBookTestCase(unittest.TestCase):
         minute = 20
         sec = 0.0
         act_jd = astro_time.julian_date(yr, month, day, hr, minute, sec)
-        print(act_jd)
         exp_jd = 2450383.09722222
         self.assertAlmostEqual(act_jd, exp_jd, 8)
+        
+    def test_example_3_5_gmst_lst(self):
+        month = 8
+        day = 20
+        yr = 1992
+        hr = 12
+        minute = 14
+        sec = 0.0
+        lon = -104.0
+        
+        jd_ut1 = astro_time.julian_date(yr, month, day, hr, minute, sec)
+        act_theta_gmst = astro_time.find_gmst(jd_ut1)
+        act_theta_lst = astro_time.find_lst(act_theta_gmst, lon)
+        
+        exp_theta_gmst = 152.578787810
+        exp_theta_lst = 48.578787810
+        
+        self.assertAlmostEqual(act_theta_gmst, exp_theta_gmst)
+        self.assertAlmostEqual(act_theta_lst, exp_theta_lst)
+        
         
         
 def test_main():
